da0dd143501956efa308339e5b5936bf485402d9
Alex Morales <am8850mia@gmail.com>
2023-05-30 13:58:09 -0400

    Cleanup

--- acli/main.go
+++ acli/main.go
@@ -6,14 +6,6 @@ import (
 )
 
 func main() {
-	//services.CheckArgs("<url>", "<directory>")
-	//url := os.Args[1]
-	//directory := os.Args[2]
-	//services.Clone(url, directory)
-
-	// acli init project-name --template vite-go --git-init
-	// acli templates
-
 	err := cmd.Execute()
 	if err != nil {
 		log.Fatalln("Error executing acli: ", err)
