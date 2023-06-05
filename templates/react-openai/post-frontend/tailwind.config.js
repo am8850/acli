fb161ac3b250bfb9febecd690daadaa8d1e5137c
Alex Morales <am8850mia@gmail.com>
2023-06-04 20:37:09 -0400

    Update tailwind.config.js

--- templates/react-openai/post-frontend/tailwind.config.js
+++ templates/react-openai/post-frontend/tailwind.config.js
@@ -4,14 +4,10 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin'),
-    require('@tailwindcss/forms'),
   ],
 }
-
